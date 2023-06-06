52a1f84a8a4131edbeafb0750122ee49f17b7be9
Jdubbs702 <702leopold@gmail.com>
2023-06-06 11:17:10 +0300

    set up signup, login, getUser, updateUser

---
+++ server/controllers/users-controller.js
@@ -0,0 +1,138 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const UserModel = require("../models/users");
+const DB = require("../db");
+const HttpError = require("../models/http-error");
+
+const usersDB = new DB(UserModel);
+const secretCode = "super_secret_dont_share";
+
+//create
+const userSignup = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new HttpError("Invalid inputs, please check yo-self", 422);
+    }
+
+    const { firstName, lastName, phone, email, password } = req.body;
+
+    const existingUser = await usersDB.find({ email: email });
+    if (existingUser) {
+      throw new HttpError("Email already exists", 422);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const constructedUser = {
+      firstName,
+      lastName,
+      phone,
+      email,
+      password: hashedPassword,
+    };
+    const newUser = await usersDB.add(constructedUser);
+
+    const token = jwt.sign(
+      { userId: newUser.id, email: newUser.email },
+      secretCode,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ user: newUser, token: token });
+  } catch (error) {
+    next(new HttpError("Signup failed. Please try again later.", 500));
+  }
+};
+
+const userLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const identifiedUser = await usersDB.find({ email: email });
+    if (!identifiedUser) {
+      throw new HttpError("Credentials are incorrect", 403);
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      password,
+      identifiedUser.password
+    );
+    if (!isValidPassword) {
+      throw new HttpError("Credentials are incorrect", 403);
+    }
+
+    const token = jwt.sign(
+      { userId: identifiedUser.id, email: identifiedUser.email },
+      secretCode,
+      { expiresIn: "1h" }
+    );
+
+    res.json({ user: identifiedUser, token: token });
+  } catch (error) {
+    next(new HttpError("Login failed. Please try again later.", 500));
+  }
+};
+
+//read
+const getUserById = async (req, res, next) => {
+  try {
+    const user = await usersDB.getById(req.params.userId);
+    if (!user) {
+      throw new HttpError("Could not find a user for the provided id", 404);
+    }
+    res.json({ user });
+  } catch (error) {
+    next(new HttpError("Failed to fetch user. Please try again later.", 500));
+  }
+};
+
+//update
+const updateUserById = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError("Invalid inputs", 422));
+  }
+
+  const id = req.params.userId;
+  const newItem = req.body;
+
+  try {
+    const hasUser = await usersDB.find({ email: newItem.email });
+
+    if (hasUser && hasUser._id !== id) {
+      throw new HttpError("Email is already in use", 422);
+    }
+
+    const updatedUser = await usersDB.update(id);
+    if (!updatedUser) {
+      throw new HttpError("Could not find a user for the provided id", 404);
+    }
+
+    const doc = updatedUser.doc;
+    doc.firstName = newItem.firstName;
+    doc.lastName = newItem.lastName;
+    doc.phone = newItem.phone;
+    doc.email = newItem.email;
+    doc.password = newItem.password;
+    if (newItem.bio) {
+      doc.bio = newItem.bio;
+    }
+
+    try {
+      updatedUser.save();
+    } catch (error) {
+      throw new HttpError("Could not update. Please try again.", 500);
+    }
+
+    res.json({ doc });
+  } catch (error) {
+    next(new HttpError("Failed to update user. Please try again later.", 500));
+  }
+};
+
+exports.userSignup = userSignup;
+exports.userLogin = userLogin;
+exports.getUserById = getUserById;
+exports.updateUserById = updateUserById;
